5e8166c21af13356f55fb321b1aec1c31f118316
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2022-06-14 16:42:18 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,106 +1,76 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+  package frc.robot;
 
-package frc.robot;
+  import edu.wpi.first.wpilibj.TimedRobot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+  import com.ctre.phoenix.motorcontrol.ControlMode; 
+  import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+  import edu.wpi.first.wpilibj.XboxController;
+  import edu.wpi.first.wpilibj.Timer;
+  import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+  import edu.wpi.first.wpilibj.Compressor; 
+  import edu.wpi.first.wpilibj.DoubleSolenoid; 
+  import edu.wpi.first.wpilibj.PneumaticsModuleType; 
+  import edu.wpi.first.wpilibj.GenericHID;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
-public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  public class Robot extends TimedRobot implements Constants {
+    protected Timer timer; 
+    protected double forwardTorque, lateralTorque; 
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+    protected final WPI_VictorSPX leftFront  = new WPI_VictorSPX(LEFT_FRONT); 
+    protected final WPI_VictorSPX rightFront = new WPI_VictorSPX(RIGHT_FRONT);   
+    protected final WPI_VictorSPX rightFollower = new WPI_VictorSPX(RIGHT_FOLLOWER); 
+    protected final WPI_VictorSPX leftFollower = new WPI_VictorSPX(LEFT_FOLLOWER); 
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
+    private final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+    private final DoubleSolenoid solenoid =  new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1); 
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    DifferentialDrive drive = new DifferentialDrive(leftFront, rightFront); 
+
+    public XboxController controller = new XboxController(JOYSTICK_PORT);  
+
+    @Override
+    public void autonomousInit() {
+      timer = new Timer(); 
+      timer.reset(); 
+      timer.start();
     }
-  }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+    @Override
+    public void autonomousPeriodic() {
+      if (timer.get() < 2.0) {
+        drive.arcadeDrive(100, 30);
+      }
+    }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void teleopInit() {
+      forwardTorque = 0; 
+      lateralTorque = 0; 
+      compressor.disable();
+    }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    @Override
+    public void teleopPeriodic() {
+      forwardTorque = controller.getRawAxis(JOYSTICK_LEFT); 
+      lateralTorque = controller.getRawAxis(JOYSTICK_RIGHT); 
+      leftFollower.follow(leftFront); 
+      rightFollower.follow(rightFront); 
+      drive.arcadeDrive(lateralTorque, forwardTorque);
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+      //Solenoid 
+      if (controller.getLeftBumper()){
+        solenoid.set(DoubleSolenoid.Value.kForward);
+      } else if (controller.getRightBumper()) {
+        solenoid.set(DoubleSolenoid.Value.kReverse); 
+      }
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+      if (controller.getAButton()){
+        compressor.enableDigital();
+      }else if (controller.getBButton()){
+        compressor.disable();
+      }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
-}
+    } 
+  } 
