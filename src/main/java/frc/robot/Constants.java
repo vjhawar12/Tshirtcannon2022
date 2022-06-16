b38288b30933697aa6b2edda047b8bd794818ce1
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2022-06-16 15:28:28 -0400

    Update Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public interface Constants { 
+    public final int JOYSTICK_RIGHT = 4;    
+    public final int JOYSTICK_LEFT = 1; 
+    public final int JOYSTICK_PORT = 0; 
+    public final int RIGHT_FRONT = 1; 
+    public final int LEFT_FRONT = 0; 
+    public final int RIGHT_FOLLOWER = 3; 
+    public final int LEFT_FOLLOWER = 2; 
+    public final double MAX_SPEED = 1.0; 
+    public final double MIN_SPEED = 0.0; 
+    public final double FORWARD = 0.0; 
+}
